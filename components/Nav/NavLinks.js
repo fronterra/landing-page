cbe5458c8d98f6701b47b93a352f3a6eff804546
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-03-20 00:19:27 -0700

    added underline to nav links

--- components/Nav/NavLinks.js
+++ components/Nav/NavLinks.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Link from "next/link";
 import { useMenuContext } from "../../state";
-export const links = ["about"];
+export const links = ["Documentation", "Deploy", "Partners"];
 
 const DesktopNavLinks = () => {
   const { closeMenu } = useMenuContext();
@@ -11,9 +11,7 @@ const DesktopNavLinks = () => {
     <NavLinksWrapper className="nav-links">
       {links.map((link) => (
         <li key={link}>
-          <NavLink href={`/${link}`} className="link" onClick={closeMenu}>
-            {link}
-          </NavLink>
+          <a href={`/${link}`} className="link">{link}</a>
         </li>
       ))}
     </NavLinksWrapper>
@@ -23,37 +21,74 @@ const DesktopNavLinks = () => {
 export default DesktopNavLinks;
 
 const NavLinksWrapper = styled.ul`
-  flex: 1;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  list-style: none;
-  li:not(:last-child) {
-    margin-right: 26px;
-  }
-  li:last-child {
-    margin-left: auto;
-  }
-  button {
-    background: white;
-    outline: none;
-    border: none;
-    cursor: pointer;
+  justify-content: space-between;
+  flex-direction: flex-end;
+  align-items: flex-end;
+  li {
+    font-weight: 500;
+    padding: 30px;
+    font-size: 1.25rem;
+    a {
+      font-size: 20px;
+      color: black;
+      position: relative;
+      text-decoration: none;
+      padding-bottom: 8px;
+    
+       &:before,
+      &:after {
+        content: '';
+        position: absolute;
+        bottom: 2px;
+        left: 0; right: 0;
+        height: 2px;
+        background-color: black;
+      }
+      &:before {
+        opacity: 0;
+        transform: translateY(- 8px);
+        transition: transform 0s cubic-bezier(0.175, 0.885, 0.320, 1.275), opacity 0s;
+      }
+      &:after {
+        opacity: 0;
+        transform: translateY(8px/2);
+        transition: transform .3s cubic-bezier(0.175, 0.885, 0.320, 1.275), opacity .3s;
+      }
+      &:hover,
+      &:focus {
+        &:before,
+        &:after {
+          opacity: 1;
+          transform: translateY(0);
+        }
+        &:before {
+          transition: transform .3s cubic-bezier(0.175, 0.885, 0.320, 1.275), opacity .3s;
+        }
+        &:after {
+          transition: transform 0s .3s cubic-bezier(0.175, 0.885, 0.320, 1.275), opacity 0s .3s;
+        }
+      }
+    }
   }
   @media screen and (max-width: 768px) {
     flex-direction: column;
+    justify-content: center;
+    flex-direction: center;
+    align-items: center;
     li {
-      padding: 12px;
+      font-weight: 400;
+      font-size: 1.40rem;
+      padding: 20px;
       margin: 0 !important;
+
     }
   }
 `;
 
 export const NavLink = styled(Link)`
   position: relative;
-  color: white;
   text-decoration: none;
-  text-transform: capitalize;
   color: black;
   &::before {
     content: "";
