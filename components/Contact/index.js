7e48517dc540f0f9ba9b8645bbc87b37d9f997ed
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-07 17:48:21 -0700

    added new contact page components

---
+++ components/Contact/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ContactContainer, ContactContent, ContactH2 } from './ContactElements';
+
+const Contact = () => {
+    return (
+        <>
+            <ContactContainer>
+                <ContactContent>
+                    <ContactH2>
+                        Contact Us
+                    </ContactH2>
+                </ContactContent>
+            </ContactContainer>
+        </>
+    );
+}
+
+export default Contact;
