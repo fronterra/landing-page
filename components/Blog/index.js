970384c991e308170d62d66c28c5e0ce8cec4cac
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-27 17:21:10 -0700

    added blog component boilerplate

---
+++ components/Blog/index.js
@@ -0,0 +1,14 @@
+import { BlogContainer } from "./BlogStyles"
+import BlogHeader from "./BlogHeader"
+
+const Blog = () => {
+    return (
+        <>
+            <BlogContainer>
+                <BlogHeader />
+            </BlogContainer>
+        </>
+    )
+};
+
+export default Blog;
