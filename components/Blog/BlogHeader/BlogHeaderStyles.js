91a33337c424b78c40c84f0c8e579bebbdd7b547
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-27 17:21:19 -0700

    added blog header styles

---
+++ components/Blog/BlogHeader/BlogHeaderStyles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1``;
+
+export const AuthorName = styled.h3``;
+
+export const PublicationDate = styled.h4``;
+
+export const Description = styled.h5``;
+
+export const BlogHeaderContainer = styled.div``;
