b74a6453be1324f1dab3a2785315fe48f6ba80ec
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-28 13:12:59 -0700

    added MDX loaders to next configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 const withImages = require('next-images');
+const withMDX = require('@next/mdx')({
+    extension: /\.mdx$/
+});
 const withPlugins = require('next-compose-plugins');
 
-module.exports = withPlugins([[withImages]]);
+module.exports = withPlugins(
+    [
+        [withImages], 
+        [withMDX]
+    ],
+    {
+        pageExtensions: ['js', 'jsx', 'mdx']
+    }
+);
