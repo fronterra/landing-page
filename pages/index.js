94833aab8fd33f17f412a23bf2ac326a5c56e208
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-30 18:49:00 -0700

    added meta tags to head element

--- pages/index.js
+++ pages/index.js
@@ -30,13 +30,16 @@ export default function Home() {
 
     <>
       <Head>
+        <title>Pollution Project</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Crowdsourced solutions to pollution reporting" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;600;900&display=swap" rel="stylesheet" />
       </Head>
-      <Navbar toggle={toggle} isOpen={isOpen}/>
-      <Sidebar toggle={toggle} isOpen={isOpen} />   
+      {/* <Navbar toggle={toggle} isOpen={isOpen}/>
+      <Sidebar toggle={toggle} isOpen={isOpen} />    */}
       <HeroSection />
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
