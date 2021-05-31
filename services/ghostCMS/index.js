31c9ed1326caef287d9c9f71008e8a0661de0b3c
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-30 18:45:30 -0700

    added ghost content api handler

---
+++ services/ghostCMS/index.js
@@ -0,0 +1,40 @@
+import GhostContentAPI from "@tryghost/content-api";
+
+export default (function ghostHandler () {
+    // create ghost content api client instance
+    const api = new GhostContentAPI({
+        url: process.env.GHOST_URL,
+        key: process.env.GHOST_API_KEY,
+        version: process.env.GHOST_API_VERSION
+    })
+
+
+    return {
+        /** */
+        async getPosts({ limit = null }) {
+            try {
+
+                const config = {};
+                if (limit !== null && typeof limit === 'number') {
+                    // set `limit` arg into config if deemed valid
+                    config.limit = limit;
+                }
+                
+                // returns an array of post objects
+                return (await api.posts.browse({ ...config }));
+
+            } catch (err) {
+                // handle error
+                throw err;
+            }
+        },
+        /** */
+        async getPostBySlug(slug) {
+            try {
+                return (await api.posts.read({ slug }));
+            } catch (err) {
+                throw err;
+            }
+        },
+    }
+})();
