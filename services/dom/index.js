e162953c6e63d7daaa4a2337bdccd4d81ee89aac
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-31 12:41:14 -0700

    refact fn to use cheerio().html(), not DOMparser

--- services/dom/index.js
+++ services/dom/index.js
@@ -1,14 +1,15 @@
-
+import cheerio from 'cheerio'; // module to handle DOM parsing from nodeJS runtime
 
 /**
  * Convert a template string into HTML DOM nodes
  * @param {String} string the template string
  * @return {Node} the template node
  * 
- * See https://gomakethings.com/converting-a-string-into-markup-with-vanilla-js/ for reference
  */
 export const stringToHTML = (string) => {
-    let parser = new DOMParser();
-    let document = parser.parseFromString(string, 'text/html');
-    return document.body;
+    // load string into cheerio
+    const $ = cheerio.load(string);
+
+    // return rendered HTML document
+    return $.html();
 };
