91a92efe3f7a4b8b0c18cc3d1605ca2f6c28aa06
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-30 18:46:26 -0700

    moved navbar, sidebar, and footer into _app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import { useState } from 'react'
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(() => !isOpen);
+  }
+  return (
+    <>
+      <Navbar toggle={toggle} isOpen={isOpen}/>
+      <Sidebar toggle={toggle} isOpen={isOpen} />    
+      <Component {...pageProps} />
+      <Footer />
+    </>
+
+  )
 }
 
 export default MyApp
