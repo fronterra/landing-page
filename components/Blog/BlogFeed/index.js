798ebbd17bd47850d877c106b2b4e31ab9c9107e
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-31 16:27:40 -0700

    added styled component for blog feed

---
+++ components/Blog/BlogFeed/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+const BlogFeed = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+`;
+
+export default BlogFeed;
