f90b06acb869192ecd2b3d0b65820eea25413e2d
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-31 19:10:37 -0700

    improved break points for blog feed

--- components/Blog/BlogFeed/index.js
+++ components/Blog/BlogFeed/index.js
@@ -1,11 +1,24 @@
 import styled from 'styled-components';
-const BlogFeed = styled.div`
+
+
+
+export const BlogFeedContainer = styled.div`
     width: 100%;
     height: 100%;
     display: flex;
     justify-content: center;
-    flex-direction: row;
-    flex-wrap: wrap;
 `;
 
-export default BlogFeed;
+export const BlogFeed = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    flex-wrap: wrap;
+    max-width: 1100px;
+
+    @media only screen and (max-width: 1100px) {
+        max-width: 90%;
+    }
+`;
