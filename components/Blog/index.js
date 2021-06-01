3da6929bb171a4e05ecb09126e168fdb0d3672a7
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-31 19:10:02 -0700

    added blog home page container styles

--- components/Blog/index.js
+++ components/Blog/index.js
@@ -1,14 +1,28 @@
-import { BlogContainer } from "./BlogStyles"
-import BlogHeader from "./BlogHeader"
+import styled from 'styled-components';
 
-const Blog = () => {
-    return (
-        <>
-            <BlogContainer>
-                <BlogHeader />
-            </BlogContainer>
-        </>
-    )
-};
+export const BlogHomeHeaderContainer = styled.div`
+    height: 300px;
+    width: 100%;
 
-export default Blog;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+    text-align: center;
+    margin: auto;
+    max-width: 1100px;
+`;
+
+export const BlogHomeHeader = styled.div`
+    margin-top: 100px;
+    height: 100%;
+    align-content: center;
+    justify-content: center;
+    width: 90%;
+    text-align: start;
+`;
+
+export const StyledH1 = styled.h1`
+    font-size: 40px;
+`;
