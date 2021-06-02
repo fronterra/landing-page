077bca3493acf87158f26a55db7a5d0c68235544
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-06-02 16:01:19 -0700

    rm refs to mdx loader from next config file

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 const withImages = require('next-images');
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx$/
-});
 const withPlugins = require('next-compose-plugins');
 
 module.exports = withPlugins(
     [
-        [withImages], 
-        [withMDX]
+        [withImages],
     ],
     {
-        pageExtensions: ['js', 'jsx', 'mdx'],
-        target: "serverless"
+        target: "serverless",
     }
 );
