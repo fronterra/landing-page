8aa77133bfc5ac65da7ccc36edad331ee2144b65
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-06-01 22:31:48 -0700

    replacing logo png for imported font family logo

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="Crowdsourced solutions to pollution reporting" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="stylesheet" href="https://use.typekit.net/kgt4trk.css"></link>
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;600;900&display=swap" rel="stylesheet" />
       </Head>
       <HeroSection />
