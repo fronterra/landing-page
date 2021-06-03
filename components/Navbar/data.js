52264992fd0ba61ae1418452f11d18893edaaa0d
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-06-03 16:45:18 -0700

    moved menu data into own file within nav scope

---
+++ components/Navbar/data.js
@@ -0,0 +1,31 @@
+export const menuData = {
+    keys: ['github', 'digest'],
+    values: {
+        documentation: {
+            display: "Documentation",
+            id: "documentation",
+            href: "/documentation"
+        },
+        fronterra: {
+            display: "FronTERRA",
+            id: "fronterra",
+            href: "/fronterra"
+        },
+        digest: {
+            display: "Digest",
+            id: "digest",
+            href: "/digest"
+        },
+        contact: {
+            display: "Contact",
+            id: "contact",
+            href: "/contact"
+        },
+        github: {
+            display: "GitHub",
+            id: "github",
+            href: "https://github.com/pollution-project",
+            isExternal: true
+        }
+    }
+};
